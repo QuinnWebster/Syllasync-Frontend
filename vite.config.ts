92e711b01ead9ff121371bb21a15c6cbb20748d7
Quinn Webster <qwebster737@gmail.com>
2024-11-18 16:02:58 -0800

    Updated vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        // Ignore unused external import warnings
+        if (warning.code === "UNUSED_EXTERNAL_IMPORT") return;
+        // Print other warnings
+        warn(warning);
+      },
+    },
+  },
+});
